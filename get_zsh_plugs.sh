6dec51bfb39086e8bf274f54863377c4047983f1
GCNull <46764501+GCNull@users.noreply.github.com>
2022-12-05 00:11:33 +0000

    Update get_zsh_plugs.sh

--- get_zsh_plugs.sh
+++ get_zsh_plugs.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/ohmyzsh/ohmyzsh
-git clone https://github.com/marlonrichert/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
 
 mv ohmyzsh $HOME/.local/share/zsh_conf
-mv zsh-autocomplete $HOME/.local/share/zsh_conf
+mv zsh-autosuggestions $HOME/.local/share/zsh_conf
 mv zsh-syntax-highlighting $HOME/.local/share/zsh_conf
